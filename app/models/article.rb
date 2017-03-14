26243be445c0fe2617737f7ba95f46f89b6b343f
Shaun Carland <scarland@yesware.com>
2017-03-13 23:04:13 -0400

    Add attr_assessor and validations to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
-class Article < ApplicationRecord
+class Article < ApplicationRecord::Base
+  attr_accessible :title, :datetime, :content
+  validates :title, :presence => true
+  validates :datetime, :presence => true
+  validates :content, :presence => true
 end
